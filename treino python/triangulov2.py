428e4fb1b65f79464a59ea27024f649c5c68e598
Gabriel Maiolli <maiolligabriel@gmail.com>
2024-06-19 22:53:04 -0300

    .

--- treino python/triangulov2.py
+++ treino python/triangulov2.py
@@ -16,5 +16,3 @@ if b - c < a < b + c and a - c < b < a + c and b - a < c < b + a:
         print("As retas {} {} {} formam um triangulo escaleno".format(a, b, c))
 else:
     print("nao eh um triangulo")
-
-#teste
